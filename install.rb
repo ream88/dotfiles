221835cb1d673fa4707068064d6c00ab1b705864
Mario Uher <uher.mario@gmail.com>
2012-03-16 12:41:31 +0100

    Moved dotfiles.

--- install.rb
+++ install.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 
-Dir.chdir(File.dirname(__FILE__)) do
-  Dir.entries(Dir.pwd).grep(/^[^\.]+$/).each do |file|
+Dir.chdir(File.join(File.dirname(__FILE__), 'dotfiles')) do
+  Dir.glob('*').each do |file|
     source = File.join(Dir.pwd, file)
     destination = File.join(ENV['HOME'], ".#{file}")
     
-    system("ln -vs #{source} #{destination}") unless File.exist?(destination)
+    system("ln -vsf #{source} #{destination}")
   end
 end
--- uninstall.rb
+++ uninstall.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
-Dir.chdir(File.dirname(__FILE__)) do
-  Dir.entries(Dir.pwd).grep(/^[^\.]+$/).each do |file|
+Dir.chdir(File.join(File.dirname(__FILE__), 'dotfiles')) do
+  Dir.glob('*').each do |file|
     destination = File.join(ENV['HOME'], ".#{file}")
     
-    system("rm -v #{destination}") if File.exist?(destination)
+    system("rm -v #{destination}")
   end
 end
