9ed1879f8b73632b7a442e53d5af8ec88178e521
Mario Uher <uher.mario@gmail.com>
2013-01-05 09:43:52 +0100

    Moved aliases to their own file.

---
+++ dotfiles/zsh/aliases.sh
@@ -0,0 +1,17 @@
+# OS X shortcuts
+alias lock='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend'
+alias rmdsstores='find ./ -type f | grep .DS_Store | xargs rm'
+alias sleep='pmset sleepnow'
+
+
+# Git
+alias gd='git diff'
+alias gap='git add -p'
+
+
+# Programming
+alias j='jasmine-headless-webkit --color'
+
+
+# Other
+alias lisa='ls -lisa'
