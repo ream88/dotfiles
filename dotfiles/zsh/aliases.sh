b1e8a197b78f2e3eecb310c2c9f7481ff89a385b
Mario Uher <uher.mario@gmail.com>
2013-01-11 12:34:56 +0100

    Change theme of Terminal.app when executing ssh.

--- dotfiles/zsh/aliases.sh
+++ dotfiles/zsh/aliases.sh
@@ -15,3 +15,16 @@ alias j='jasmine-headless-webkit --color'
 
 # Other
 alias lisa='ls -lisa'
+
+# Change theme of Terminal.app
+tabc() {
+  NAME="${1:-Default}"
+  osascript -e "tell application \"Terminal\" to set current settings of front window to settings set \"$NAME\""
+}
+
+# Change to Danger theme when executing ssh
+ssh() {
+  tabc Danger
+  /usr/bin/ssh "$*"
+  tabc
+}
