242c798efb834465d6b34b6d6cecceda0e64018f
Mario Uher <uher.mario@gmail.com>
2013-07-16 19:58:13 +0200

    updatedb is now just an alias.

--- dotfiles/zsh/aliases.sh
+++ dotfiles/zsh/aliases.sh
@@ -18,7 +18,7 @@ alias lisa='ls -lisa'
 # Quick way to rebuild the Launch Services database and get rid
 # of duplicates in the Open With submenu.
 alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
-
+alias updatedb='sudo /usr/libexec/locate.updatedb'
 
 # gt is alias for gittower .
 gt() {
