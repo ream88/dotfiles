1a170ea7c97cae343b538f47a931b5b921322d7d
Mario Uher <uher.mario@gmail.com>
2024-05-07 11:48:50 +0200

    Improve install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
+set -e
 
-cd $HOME
-git clone https://github.com/ream88/dotfiles.git ~/.dotfiles
+if [ ! -d "$HOME/.dotfiles" ]; then
+  git clone https://github.com/ream88/dotfiles.git $HOME/.dotfiles
+fi
 
-for file in $HOME/.dotfiles/src/*; do
-  basename=$(basename "$file")
-  from="$HOME/.dotfiles/src/$basename"
-  to="$HOME/.$basename"
-  ln -sfvn "$from" "$to"
-done
+install() {
+  for from in $1/*; do
+    basename=$(basename $from)
+
+    mkdir -pv $2
+
+    if [ -d $from ]; then
+      install $from "$2$basename/"
+    else
+      echo ln -sfvn $from "$2$basename"
+    fi
+  done
+}
+
+install "$HOME/.dotfiles/src" "$HOME/."
